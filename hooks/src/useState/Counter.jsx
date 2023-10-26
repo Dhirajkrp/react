3a81d7f13fb6be722062515b1912445587f96f5f
Dhiraj <iamdhirajkrprasad@gmail.com>
2023-10-26 13:32:36 +0530

    starting with the usestate hook

--- hooks/src/useState/Counter.jsx
+++ hooks/src/useState/Counter.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 function Counter() {
   //step 2,3: initialize the state ,and create a function to set the state,
   const [count, setCount] = useState(0);
+  // providing the button for the state change.
   return <button onClick={() => setCount(count + 1)}>Count {count}</button>;
 }
 
