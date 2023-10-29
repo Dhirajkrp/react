a8b86032f7d2271847e3c2a699bb77fbfe9d7ccb
Dhiraj <iamdhirajkrprasad@gmail.com>
2023-10-29 14:05:52 +0530

    finishing up with the static layout of the project

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -5,6 +5,8 @@ import Product from "./pages/Product";
 import HomePage from "./pages/HomePage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 import PageNav from "./components/PageNav";
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
+          <Route path="/app" element={<AppLayout />} />
+          <Route path="/login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
