f5c52637957992c80be565c3dd79146f54204723
Dhiraj <iamdhirajkrprasad@gmail.com>
2023-11-02 13:55:40 +0530

    creating a custom hook for accessing the context

--- atomic-post/src/PostsContext.js
+++ atomic-post/src/PostsContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -49,4 +49,15 @@ function PostProvider({ children }) {
   );
 }
 
-export { PostContext, PostProvider };
+//creating a custom hook for using the context.
+
+function usePosts() {
+  const context = useContext(PostContext);
+
+  if (context === undefined) {
+    throw new Error("PostContext was used outside the PostProvider");
+  }
+  return context;
+}
+
+export { PostProvider, usePosts };
