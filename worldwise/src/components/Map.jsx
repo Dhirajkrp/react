4f1c76995539ca4552c5d03e68e9feee4eee4a21
Dhiraj <iamdhirajkrprasad@gmail.com>
2024-01-15 10:41:18 +0530

    setting up the leaflet map api

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./Map.module.css";
 
 import { useNavigate, useSearchParams } from "react-router-dom";
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -10,6 +12,7 @@ function Map() {
   const lng = searchParams.get("lng");
 
   const navigate = useNavigate();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
   return (
     <div className={styles.mapContainer} onClick={() => navigate("/app/form")}>
       Map
@@ -19,6 +22,22 @@ function Map() {
       <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
         Change Position
       </button>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
